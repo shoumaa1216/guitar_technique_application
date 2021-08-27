112539fd54f431d4e251ad119410ee9d844b3e09
shoumaa1216 <shoumaa1216@gmail.com>
2021-08-27 09:49:08 +0900

    onomatopoeiasテーブルレコード追加

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        $this->call(TechniquesTableSeeder::class);
+       $this->call(OnomatopoeiasTableSeeder::class);
     }
 }
---
+++ database/seeds/OnomatopoeiasTableSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OnomatopoeiasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table("onomatopoeias")->insert(
+      [
+        [
+          'id'=>'1',
+          'technique_id'=>'1',
+          'name'=>'チュイーン',
+        ],
+        [
+          'id'=>'2',
+          'technique_id'=>'2',
+          'name'=>'チューンウンウン',
+        ],
+        [
+          'id'=>'3',
+          'technique_id'=>'3',
+          'name'=>'キンコーン',
+        ],
+        [
+         'id'=>'4',
+         'technique_id'=>'4',
+         'name'=>'キュイーン',
+        ],
+         [
+          'id'=>'5',
+          'technique_id'=>'5',
+          'name'=>'ギュイーン',
+        ],
+        [
+          'id'=>'6',
+          'technique_id'=>'6',
+          'name'=>'ギー',
+        ],
+        [
+          'id'=>'7',
+          'technique_id'=>'7',
+          'name'=>'ズンズン',
+        ],
+        [
+         'id'=>'8',
+         'technique_id'=>'8',
+         'name'=>'チャカチャカ',
+        ],
+         [
+          'id'=>'9',
+          'technique_id'=>'9',
+          'name'=>'チューン',
+        ],
+        [
+          'id'=>'10',
+          'technique_id'=>'10',
+          'name'=>'ミョーン',
+        ],
+        [
+          'id'=>'11',
+          'technique_id'=>'11',
+          'name'=>'ニュン',
+        ],
+        [
+         'id'=>'12',
+         'technique_id'=>'12',
+         'name'=>'リルリル',
+        ],
+        ]
+      );
+    }
+}
