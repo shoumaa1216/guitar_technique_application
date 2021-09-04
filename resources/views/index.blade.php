6f0ece6bf4c6d705ffa2b647fea996e1851229a1
shoumaa1216 <shoumaa1216@gmail.com>
2021-09-04 14:38:18 +0900

    index.blade.phpフォーム追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,17 @@
  <h1 class="title">ギターテクニック擬音検索</h1>
  <p class="explanation">このサイトはギターの擬音を入力することで、ギターのテクニックを検索することができます。下のフォームに擬音を入力してください。(例:チュイーン、ギュオーン)</p> 
  <form action="result" method="POST" class="result_form">
-     <div class="result_form">
      {{ csrf_field() }}
+     <div class="result_form">
       <input type="text" name="onomatopoeia[name]" id="text">
       <input type="submit" value="検索">
       </div>
   </form>
+ <form action="register" method="get" class="register_form">
+      {{ csrf_field() }}
+      <div class="register_form">
+       <input type="submit" value="擬音登録ページ">   
+      </div>
+ </form>
 </body>
 </html>
